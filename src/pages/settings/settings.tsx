9c29de7dd5e124bb2bd43d178d26e4c1cbf1bf5c
ryabykhms <ryabykhms@gmail.com>
2020-12-29 18:58:10 +0300

    feat: change settings component for choose language

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,5 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { setLanguage } from '@store';
 import './settings.css';
 
 export const Settings = () => {
-  return <div>Settings</div>;
+  const dispatch = useDispatch();
+  const setLang = (lang: string) => dispatch(setLanguage(lang));
+
+  return (
+    <div>
+      <div>
+        <span>Select language: </span>
+        <button onClick={() => setLang('ru')}>ru</button>
+        <button onClick={() => setLang('en')}>en</button>
+      </div>
+    </div>
+  );
 };

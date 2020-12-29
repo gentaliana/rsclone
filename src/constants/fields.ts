0ac91a8bc0350275a01f24baa106a41eb13b9f65
ryabykhms <ryabykhms@gmail.com>
2020-12-30 02:00:54 +0300

    RC-1 fix(fields): change FieldSizes enum for right keys format

--- src/constants/fields.ts
+++ src/constants/fields.ts
@@ -1,11 +1,11 @@
 export enum FieldSizes {
-  'THREE' = 3,
-  'FOUR' = 4,
-  'FIVE' = 5,
-  'SIX' = 6,
-  'SEVEN' = 7,
-  'EIGHT' = 8,
-  'NINE' = 9,
+  THREE = 3,
+  FOUR = 4,
+  FIVE = 5,
+  SIX = 6,
+  SEVEN = 7,
+  EIGHT = 8,
+  NINE = 9,
 }
 
 export const DEFAULT_FIELD_SIZE = FieldSizes.FIVE;

6a803b5c1d22b1305e44b8e6498530cfa8a607c4
ryabykhms <ryabykhms@gmail.com>
2020-12-29 18:55:59 +0300

    fix: change lang selector for new state structure

--- src/pages/set-game/set-game.tsx
+++ src/pages/set-game/set-game.tsx
@@ -3,7 +3,7 @@ import { IAppState } from '@types';
 import './set-game.css';
 
 export const SetGame = () => {
-  const lang = useSelector((state: IAppState) => state.lang);
+  const lang = useSelector((state: IAppState) => state.settings.lang);
 
   return <div>SetGame {lang}</div>;
 };

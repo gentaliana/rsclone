ff1dd6c73c512325a7ba012ba77891ee4ca7bbe2
gentaliana <gentaliana@gmail.com>
2021-01-01 22:28:56 +0300

    RC-27 feat(all): add i18n

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from '@constants';
+import { useTranslation } from 'react-i18next';
 import './main.scss';
 
-export const Main = (): JSX.Element => (
-  <ul>
-    <li>
-      <Link to={routes.SET_GAME}>New Game</Link>
-    </li>
-    <li>
-      <Link to={routes.RATING}>Rating</Link>
-    </li>
-    <li>
-      <Link to={routes.SETTINGS}>Settings</Link>
-    </li>
-    <li>
-      <Link to={routes.ABOUT}>About</Link>
-    </li>
-  </ul>
-);
+export const Main = (): JSX.Element => {
+  const { t } = useTranslation();
+  return (
+    <ul>
+      <li>
+        <Link to={routes.SET_GAME}>{t('links.new-game')}</Link>
+      </li>
+      <li>
+        <Link to={routes.RATING}>{t('links.rating')}</Link>
+      </li>
+      <li>
+        <Link to={routes.SETTINGS}>{t('links.settings')}</Link>
+      </li>
+      <li>
+        <Link to={routes.ABOUT}>{t('links.about')}</Link>
+      </li>
+    </ul>
+  );
+};

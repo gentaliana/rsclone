ccfec593e9227e406cd0b71c5d79edd1b7177848
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2021-01-05 13:01:46 +0300

    RC-47 refactor(all): remove console.log

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -13,7 +13,6 @@ export const useKeyPress = (targetKey: string): boolean => {
 
   // If released key is our target key then set to false
   const upHandler = ({ key }: { key: string }) => {
-    console.log(key);
     if (key === targetKey) {
       setKeyPressed(false);
     }

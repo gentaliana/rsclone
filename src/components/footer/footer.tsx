43ae70456aa52e7ec2a125a6376ac979560df525
gentaliana <gentaliana@gmail.com>
2021-01-07 18:10:23 +0300

    RC-74 feat(footer): added footer svg and names

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import logo from '../../assets/icons/rs_school_js.svg';
 import './footer.scss';
 
 export const Footer = (): JSX.Element => {
@@ -17,16 +18,27 @@ export const Footer = (): JSX.Element => {
             &nbsp;
           </span>
           <span>
-            {t('footer.and')}
-            &nbsp;
             <a href="https://github.com/gentaliana" target="_blank" rel="noreferrer">
               Gentaliana
+            </a>
+            ,
+          </span>
+          <span>
+            <a href="https://github.com/ryabykhms" target="_blank" rel="noreferrer">
+              &nbsp;Ryabykhms
+            </a>
+            ,
+          </span>
+          <span>
+            &nbsp;
+            <a href="https://github.com/Kulgeyko93" target="_blank" rel="noreferrer">
+              Kulgeyko93
             </a>
           </span>
         </div>
         <span>
           <a href="https://rs.school/js/" target="_blank" rel="noreferrer">
-            {/* <img alt="logo" className="logo" src={logo} /> */}
+            <img alt="logo" className="logo" src={logo} />
           </a>
           2020
         </span>

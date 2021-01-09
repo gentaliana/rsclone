9bb6b50795b0f7d47a1138331658d104257829a0
gentaliana <gentaliana@gmail.com>
2021-01-09 19:11:41 +0300

    RC-52 feat(auth): add register

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -4,7 +4,12 @@ import { TextInput } from '@components';
 import { Button } from 'react-bootstrap';
 import './auth.scss';
 
-export const Auth = (): JSX.Element => {
+interface IAuthProps {
+  isRegister?: boolean;
+}
+
+export const Auth = (props: IAuthProps): JSX.Element => {
+  const { isRegister } = props;
   const { t } = useTranslation();
   const [login, setLogin] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -12,14 +17,51 @@ export const Auth = (): JSX.Element => {
   return (
     <div className="auth__wrapper">
       <TextInput placeholder={t('auth.login')} classInput="auth__input auth__style" value={login} onChange={setLogin} />
-      <TextInput
+      {/* <TextInput
         placeholder={t('auth.password')}
         classInput="auth__input auth__style"
         value={password}
         onChange={setPassword}
         type="password"
-      />
-      <Button variant="success">{t('auth.entry')}</Button>
+      /> */}
+      {isRegister ? (
+        <>
+          <TextInput
+            placeholder={t('auth.password')}
+            classInput="auth__input auth__style"
+            value={password}
+            onChange={setPassword}
+            type="password"
+          />
+          <TextInput
+            placeholder={t('auth.confirm')}
+            classInput="auth__input auth__style"
+            value={password}
+            onChange={setPassword}
+            type="password"
+          />
+          <Button className="auth__button" variant="success">
+            {t('auth.register')}
+          </Button>
+        </>
+      ) : (
+        <>
+          <TextInput
+            placeholder={t('auth.password')}
+            classInput="auth__input auth__style"
+            value={password}
+            onChange={setPassword}
+            type="password"
+          />
+          <Button className="auth__button" variant="success">
+            {t('auth.entry')}
+          </Button>
+        </>
+      )}
     </div>
   );
+};
+
+Auth.defaultProps = {
+  isRegister: true,
 };

4bac3d8198549cd43d8062b945755cbd9dd5a66c
gentaliana <gentaliana@gmail.com>
2021-01-16 19:43:20 +0300

    RC-61 feat(home): add style home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -8,19 +8,19 @@ export const Home = (): JSX.Element => {
   const { t } = useTranslation();
   return (
     <>
-      <ul className="main">
-        <li>
-          <Link to={routes.SET_GAME}>{t('links.new-game')}</Link>
-        </li>
-        <li>
-          <Link to={routes.RATING}>{t('links.rating')}</Link>
-        </li>
-        <li>
-          <Link to={routes.SETTINGS}>{t('links.settings')}</Link>
-        </li>
-        <li>
-          <Link to={routes.ABOUT}>{t('links.about')}</Link>
-        </li>
+      <ul className="main home__wrapper">
+        <Link to={routes.SET_GAME}>
+          <li className="home__menu">{t('links.new-game')}</li>
+        </Link>
+        <Link to={routes.RATING}>
+          <li className="home__menu">{t('links.rating')}</li>
+        </Link>
+        <Link to={routes.SETTINGS}>
+          <li className="home__menu">{t('links.settings')}</li>
+        </Link>
+        <Link to={routes.ABOUT}>
+          <li className="home__menu">{t('links.about')}</li>
+        </Link>
       </ul>
     </>
   );

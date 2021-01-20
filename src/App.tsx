65e6e0ce3e7119c7251c48c4c19a61ddcb089e1a
gentaliana <gentaliana@gmail.com>
2021-01-20 19:27:22 +0300

    RC-150 feat(all): add style footer theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,38 @@ import { Route, Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Home, NotFound, SetGame, Game, Rating, Settings, About, Auth } from '@pages';
 import { Header, Footer, Alerts } from '@components';
-import { routes } from '@constants';
+import { routes, Theme } from '@constants';
 import { IAppState } from '@types';
 import './App.scss';
 
 function App(): JSX.Element {
   const notify = useSelector((state: IAppState) => state.notify);
+  const themeInitial = useSelector((state: IAppState) => state.settings.currentTheme);
+
+  const themeChange = React.useMemo(
+    () => `background ${themeInitial === Theme.light ? 'background_light' : 'background_dark'}`,
+    [themeInitial],
+  );
 
   return (
-    <div className="wrapper">
-      {notify ? <Alerts notify={notify} /> : null}
-      <Header />
-      <main className="main">
-        <Switch>
-          <Route exact path={routes.AUTH} component={Auth} />
-          <Route exact path={routes.HOME} component={Home} />
-          <Route path={routes.SET_GAME} component={SetGame} />
-          <Route path={routes.GAME} component={Game} />
-          <Route path={routes.RATING} component={Rating} />
-          <Route path={routes.SETTINGS} component={Settings} />
-          <Route path={routes.ABOUT} component={About} />
-          <Route component={NotFound} />
-        </Switch>
-      </main>
-      <Footer />
+    <div className={themeChange}>
+      <div className="wrapper">
+        {notify ? <Alerts notify={notify} /> : null}
+        <Header />
+        <main className="main">
+          <Switch>
+            <Route exact path={routes.AUTH} component={Auth} />
+            <Route exact path={routes.HOME} component={Home} />
+            <Route path={routes.SET_GAME} component={SetGame} />
+            <Route path={routes.GAME} component={Game} />
+            <Route path={routes.RATING} component={Rating} />
+            <Route path={routes.SETTINGS} component={Settings} />
+            <Route path={routes.ABOUT} component={About} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
